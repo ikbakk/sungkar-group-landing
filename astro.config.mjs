f4a2fe3b3c5fce2ce2255e4a3d5eb9512c3f3ae1
ikbakk <ikbakfir@gmail.com>
2026-06-08 13:29:08 +0800

    refactor: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
-import tunnel from "astro-tunnel";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [
-    react(),
-    tunnel({
-      url: "http://0.0.0.0:4321",
-      port: 4321,
-      acceptCloudflareNotice: true,
-    }),
-  ],
+  integrations: [react()],
 });
