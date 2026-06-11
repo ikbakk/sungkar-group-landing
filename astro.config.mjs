97e15ce1bda6e8e66cd57755098f626bf15eeedb
ikbakk <ikbakfir@gmail.com>
2026-06-11 19:01:10 +0800

    feat: add integration for sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import robotsTxt from "astro-robots-txt";
+
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://sungkargroup.id",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap(), robotsTxt()],
 });
