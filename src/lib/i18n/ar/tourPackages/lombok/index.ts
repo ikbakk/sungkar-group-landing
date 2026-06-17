1401dda51d0650df458aa00afa365a9b3e1603ad
ikbakk <ikbakfir@gmail.com>
2026-06-17 19:04:51 +0800

    i18n(all): add MotoGP openTrip locale files for en, ar, ms, zh
    - Create openTrip.ts with MotoGP Mandalika 2026 package for all 4 locales
    - Update lombok/index.ts in each locale to include motoGP export

--- src/lib/i18n/ar/tourPackages/lombok/index.ts
+++ src/lib/i18n/ar/tourPackages/lombok/index.ts
@@ -4,10 +4,12 @@ import type { TourPackage } from "@/lib/content/tourPackages/types";
 import { threeDays } from "./3d2n";
 import { fourDays } from "./4d3n";
 import { twoDays } from "./2d1n";
+import { motoGP } from "./openTrip";
 
 export const lombokPackages: TourPackage[] = [
   ...oneDay,
   ...twoDays,
   ...threeDays,
   ...fourDays,
+  ...motoGP,
 ];
